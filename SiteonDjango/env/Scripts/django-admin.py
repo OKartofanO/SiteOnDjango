47e38a854700a23f0b0a6a663ae157b9db65ec4d
OKartofanO <nikitos.piynov@mail.ru>
2022-09-12 00:07:44 +1000

    Первый коммит

---
+++ SiteonDjango/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\nikit\source\repos\SiteonDjango\SiteonDjango\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
